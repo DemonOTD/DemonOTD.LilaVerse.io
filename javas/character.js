5f1a55f063e69e35792677c400a6b36c759e81b7
GMcD1998 <G.mcdonald2@uni.brighton.ac.uk>
2024-10-31 17:39:18 +0000

    update2

---
+++ javas/character.js
@@ -0,0 +1,14 @@
+let character = {
+    verse: null,
+    species: null,
+    class: null,
+    abilities: {
+        strength: 0,
+        dexterity: 0,
+        constitution: 0,
+        intelligence: 0,
+        wisdom: 0,
+        charisma: 0
+    },
+    equipment: []
+};
